f7ba3a4ad1fff28c9fe7d1db9182313e7df3771d
Ruben Daniels <ruben@c9.io>
2013-12-12 21:08:04 +0000

    Added linux-x64 tmux and install script

---
+++ install.sh
@@ -0,0 +1,188 @@
+#!/bin/bash -e
+
+has() {
+  type "$1" > /dev/null 2>&1
+  return $?
+}
+
+# test for prereqs
+if ! has "curl"; then
+  echo 'Cloud9 installer needs curl to proceed.' >&2;
+fi
+
+NPM=$HOME/.c9/node/bin/npm
+NODE=$HOME/.c9/node/bin/node
+
+start() {
+  if [ $# -lt 1 ]; then
+    start base
+    return
+  fi
+  
+  # Try to figure out the os and arch for binary fetching
+  local uname="$(uname -a)"
+  local os=
+  local arch="$(uname -m)"
+  case "$uname" in
+    Linux\ *) os=linux ;;
+    Darwin\ *) os=darwin ;;
+    SunOS\ *) os=sunos ;;
+    FreeBSD\ *) os=freebsd ;;
+  esac
+  case "$uname" in
+    *x86_64*) arch=x64 ;;
+    *i*86*) arch=x86 ;;
+    *armv6l*) arch=arm-pi ;;
+  esac
+  
+  if [ $os != "linux" ] && [ $os != "darwin" ]; then
+    echo "Unsupported Platform: $os $arch" 1>&2
+    exit
+  fi
+  
+  if [ $arch != "x64" ] && [ $arch != "x86" ]; then
+    echo "Unsupported Architecture: $os $arch" 1>&2
+    exit
+  fi
+  
+  case $1 in
+    "help" )
+      echo
+      echo "Cloud9 Installer"
+      echo
+      echo "Usage:"
+      echo "    install help                       Show this message"
+      echo "    install install [name [name ...]]  Download and install a set of packages"
+      echo "    install ls                         List available packages"
+      echo
+    ;;
+
+    "ls" )
+      echo "!node - Node.js"
+      echo "!tmux - TMUX"
+      echo "!nak - TMUX"
+      echo "!ptyjs - pty.js"
+      echo "coffee - Coffee Script"
+      echo "less - Less"
+      echo "sass - Sass"
+      echo "typescript - TypeScript"
+      echo "stylus - Stylus"
+      # echo "go - Go"
+      # echo "heroku - Heroku"
+      # echo "rhc - RedHat OpenShift"
+      # echo "gae - Google AppEngine"
+    ;;
+    
+    "install" )
+      shift
+    
+      # make sure dirs are around
+      cd $HOME
+      mkdir -p .c9/bin
+      mkdir -p .c9/node_modules
+      cd .c9
+    
+      # install packages
+      while [ $# -ne 0 ]
+      do
+        eval ${1} $os $arch
+        shift
+      done
+      
+      # finalize
+      for FILE in $HOME/.c9/node_modules/.bin/* 
+      do
+          perl -i -p -e 's/#!\/usr\/bin\/env node/#!'${NODE//\//\\\/}'/' $(readlink -f $FILE)
+      done
+      
+      echo 1 > $HOME/.c9/installed
+      echo :Done.
+    ;;
+    
+    "base" )
+      echo "Installing base packages. Use --help for more options"
+      start install node tmux nak ptyjs
+    ;;
+    
+    * )
+      start base
+    ;;
+  esac
+}
+
+# NodeJS
+
+node(){
+  NODE_VERSION=v0.10.23
+  
+  # clean up 
+  rm -rf node 
+  rm -rf node-$NODE_VERSION*
+  
+  echo :Installing Node $NODE_VERSION
+  
+  curl -sSOL http://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-$1-$2.tar.gz
+  tar xvfz node-$NODE_VERSION-$1-$2.tar.gz
+  mv node-$NODE_VERSION-$1-$2 node
+  rm node-$NODE_VERSION-$1-$2.tar.gz
+}
+
+tmux(){
+  echo :Installing TMUX
+
+  rm -f ~/.c9/bin/tmux
+  ln -s `which tmux` ~/.c9/bin
+}
+
+nak(){
+  echo :Installing Nak
+  $NPM install https://github.com/c9/nak/tarball/ea1299a3688f307d2269c93bd9692101eb4f262e
+}
+
+ptyjs(){
+  echo :Installing pty.js
+  $NPM install pty.js
+}
+
+coffee(){
+  echo :Installing Coffee Script
+  $NPM install coffee
+}
+
+less(){
+  echo :Installing Less
+  $NPM install less
+}
+
+sass(){
+  echo :Installing Sass
+  $NPM install sass
+}
+
+typescript(){
+  echo :Installing TypeScript
+  $NPM install typescript  
+}
+
+stylus(){
+  echo :Installing Stylus
+  $NPM install stylus  
+}
+
+# go(){
+  
+# }
+
+# heroku(){
+  
+# }
+
+# rhc(){
+  
+# }
+
+# gae(){
+  
+# }
+
+start $@
---
+++ packages/tmux/tmux_local_static_install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Script for installing tmux on systems where you don't have root access.
+# tmux will be installed in $BASE/local/bin.
+# It's assumed that wget and a C/C++ compiler are installed.
+
+# exit on error
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE=~/.c9
+
+# create our directories
+mkdir -p $BASE/local $BASE/tmux_tmp
+cd $BASE/tmux_tmp
+
+# download source files for tmux, libevent, and ncurses
+wget -O tmux-1.6.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-1.6/tmux-1.6.tar.gz/download
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
+wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
+
+# extract files, configure, and compile
+
+############
+# libevent #
+############
+tar xvzf libevent-2.0.19-stable.tar.gz
+cd libevent-2.0.19-stable
+./configure --prefix=$BASE/local --disable-shared
+make
+make install
+cd ..
+
+############
+# ncurses  #
+############
+tar xvzf ncurses-5.9.tar.gz
+cd ncurses-5.9
+./configure --prefix=$BASE/local
+make
+make install
+cd ..
+
+############
+# tmux     #
+############
+tar xvzf tmux-1.6.tar.gz
+cd tmux-1.6
+./configure --enable-static CFLAGS="-I$BASE/local/include -I$BASE/local/include/ncurses" LDFLAGS="-static -static-libgcc -L$BASE/local/lib -L$BASE/local/include/ncurses -L$BASE/local/include"
+CPPFLAGS="-I$BASE/local/include -I$BASE/local/include/ncurses" LDFLAGS="-static -static-libgcc -L$BASE/local/include -L$BASE/local/include/ncurses -L$BASE/local/lib" make
+cp tmux $BASE/local/bin
+cd ..
+
+# cleanup
+rm -rf $BASE/tmux_tmp
+
+# package
+# Try to figure out the os and arch
+uname="$(uname -a)"
+arch="$(uname -m)"
+case "$uname" in
+  Linux\ *) os=linux ;;
+  Darwin\ *) os=darwin ;;
+  SunOS\ *) os=sunos ;;
+  FreeBSD\ *) os=freebsd ;;
+esac
+case "$uname" in
+  *x86_64*) arch=x64 ;;
+  *i*86*) arch=x86 ;;
+  *armv6l*) arch=arm-pi ;;
+esac
+
+tar -cvzf $DIR/tmux-$os-$arch.tar.gz $BASE/local
+
+echo "$BASE/local/bin/tmux is now available. The package can be found at $DIR/tmux-$os-$arch.tar.gz"
